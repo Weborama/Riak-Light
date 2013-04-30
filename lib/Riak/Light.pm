1b947d7f5b40c277e79328c4b1852a43e7a6f50b
Tiago Peczenyj <tiago.peczenyj@gmail.com>
2013-04-30 11:54:34 -0300

    first commit / use Moo

---
+++ lib/Riak/Light.pm
@@ -0,0 +1,8 @@
+## no critic (RequireUseStrict, RequireUseWarnings)
+package Riak::Light;
+## use critic
+use Moo;
+
+# ABSTRACT: Fast and lightweight Perl client for Riak
+
+1;
---
+++ t/01_load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 2;
+
+BEGIN {
+  use_ok('Riak::Light');
+}
+
+require_ok('Riak::Light')
