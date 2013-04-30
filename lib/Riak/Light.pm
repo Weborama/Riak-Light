b327b75a333bea846a9dc7ccbd93eb6a8e2d1eb3
Tiago Peczenyj <tiago.peczenyj@gmail.com>
2013-04-30 14:24:10 -0300

    refactor main class -  add _perform_request and decode message if necessary

--- lib/Riak/Light.pm
+++ lib/Riak/Light.pm
@@ -8,6 +8,8 @@ use Riak::Light::PBC;
 use JSON;
 require bytes;
 
+# ABSTRACT: Fast and lightweight Perl client for Riak
+
 has port    => (is => 'ro', isa => Int,  required => 1);
 has host    => (is => 'ro', isa => Str,  required => 1);
 has timeout => (is => 'ro', isa => Num,  default  => sub { 0.5 });
@@ -44,27 +46,11 @@ sub get {
     key => $key,
     bucket => $bucket
   });
+  my $request_code = 9;
   
-  # perform request
-  my $request_code = 9; # request 9 => GET, response 10 => GET
-  my $message      = pack( 'c', $request_code ) . $request;
-  my $len          = bytes::length($message);
-  my $operation    = pack( 'N' , $len ) . $message;
-
-  $self->socket->syswrite($operation);
-
-  # read response
-  my $code;
-  $self->socket->sysread( $len, 4 );
-  $self->socket->sysread( $code, 1 );
+  my ($code, $decoded_message) = $self->_perform_request($request_code, $request);
   
-  $len  = unpack( 'N', $len );
-  $code = unpack( 'c', $code );
-  
-  my $encoded_message;
-  $self->socket->sysread( $encoded_message, $len - 1 ) if $len > 1;
-  
-  return decode_json(RpbGetResp->decode($encoded_message)->content->[0]->value) if $code == 10;
+  return decode_json($decoded_message->content->[0]->value) if $code == 10;
   
   undef
 }
@@ -81,24 +67,9 @@ sub put {
     },
   });
   
-  # perform request
   my $request_code = 11; # request 11 => PUT, response 12 => PUT
-  my $message      = pack( 'c', $request_code ) . $request;
-  my $len          = bytes::length($message);
-  my $operation    = pack( 'N' , $len ) . $message;
-
-  $self->socket->syswrite($operation);
-
-  # read response
-  my $code;
-  $self->socket->sysread( $len, 4 );
-  $self->socket->sysread( $code, 1 );
   
-  $len  = unpack( 'N', $len );
-  $code = unpack( 'c', $code );
-  
-  my $encoded_message;
-  $self->socket->sysread( $encoded_message, $len - 1 ) if $len > 1;
+  my ($code, undef) = $self->_perform_request($request_code, $request);
   
   $code == 12
 }
@@ -112,15 +83,22 @@ sub del {
     dw => $self->rw
   });
   
-  # perform request
   my $request_code = 13; # request 13 => DEL, response 14 => DEL
+  
+  my ($code, undef) = $self->_perform_request($request_code, $request);
+  
+  $code == 14
+}
+
+sub _perform_request {
+  my ($self, $request_code, $request) = @_; 
+  
   my $message      = pack( 'c', $request_code ) . $request;
   my $len          = bytes::length($message);
   my $operation    = pack( 'N' , $len ) . $message;
 
   $self->socket->syswrite($operation);
-
-  # read response
+  
   my $code;
   $self->socket->sysread( $len, 4 );
   $self->socket->sysread( $code, 1 );
@@ -131,10 +109,13 @@ sub del {
   my $encoded_message;
   $self->socket->sysread( $encoded_message, $len - 1 ) if $len > 1;
   
-  $code == 14
+  my %decoder = (
+    0 =>  'RpbErrorResp',
+    10 => 'RpbGetResp',
+  );
+  
+  ($code, (exists $decoder{$code} and $decoder{$code}->can('decode'))? $decoder{$code}->decode($encoded_message) : undef)
 }
-
-# ABSTRACT: Fast and lightweight Perl client for Riak
 
 1;
 
